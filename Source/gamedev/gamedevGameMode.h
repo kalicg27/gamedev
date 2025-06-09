7983662fadb63b8174b57115b48663d3e3a1c1fb
kalicg27_laptop <102329198+kalicg27@users.noreply.github.com>
2025-06-09 19:27:00 +0200

    first initial

---
+++ Source/gamedev/gamedevGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gamedevGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AgamedevGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AgamedevGameMode();
+};
+
+
+
